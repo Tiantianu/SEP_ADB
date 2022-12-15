63fa57ef413e5da5137664de562ca46e288bf19f
Tiantianu <113213626+Tiantianu@users.noreply.github.com>
2022-12-14 19:08:47 -0500

    Delete Configuration.py

--- Configuration.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-username = 'tian'
-
-# COMMAND ----------
-
-moviePipelinePath=f"/dbfs/FileStore/tian/sep_movie/"
-rawPath = moviePipelinePath + "raw/"
-bronzePath = moviePipelinePath + "bronze/"
-silverPath = moviePipelinePath + "silver/"
